e252bbdce0a84a61421e7ce7bab54564f080e4e2
Rafael <ra.alvesdeazevedo@gmail.com>
2025-02-09 19:25:42 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/contador_pessoas_estabelecimento/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contador_pessoas_estabelecimento
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void decrement() {
+    setState(() {
+      count--;
+    });
+  }
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('lib/assets/images/estabelecimento.jpg'),
+        fit: BoxFit.cover,
+      )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            isFull ? 'Lotado' : 'Pode entrar!',
+            style: TextStyle(
+              fontSize: 30,
+              color: isFull ? Colors.red : Colors.white,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Text('$count',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: isFull ? Colors.red : Colors.white,
+                )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: isEmpty ? null : decrement,
+                style: TextButton.styleFrom(
+                    backgroundColor:
+                        isEmpty ? Colors.white.withOpacity(0.6) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    )),
+                child: const Text(
+                  'Saiu',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 32),
+              TextButton(
+                onPressed: isFull ? null : increment,
+                style: TextButton.styleFrom(
+                  backgroundColor:
+                      isFull ? Colors.white.withOpacity(0.6) : Colors.white,
+                  fixedSize: const Size(100, 100),
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                child: const Text(
+                  'Entrou',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
